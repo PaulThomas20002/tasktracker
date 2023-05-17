81fbbacbc66e829a5cd1de0bf8ce3e0be4ceaffe
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2023-05-17 11:39:41 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,25 +1,17 @@
-import PropTypes from 'prop-types'
+import React from 'react';
+//import PropTypes from 'prop-types'
 
-const Button = ({ color, text, onClick }) => {
+const onClick = (e) => {
+    console.log('Clicked the Button :: Add');
+}
+export default function Button( {color, text} ) {
   return (
-    <button
-      onClick={onClick}
-      style={{ backgroundColor: color }}
-      className='btn'
-    >
-      {text}
+    <button onClick={onClick} style={{ backgroundColor: color }} className="btn"> 
+    {text} 
     </button>
   )
 }
 
 Button.defaultProps = {
-  color: 'steelblue',
+    color:'SteelBlue',
 }
-
-Button.propTypes = {
-  text: PropTypes.string,
-  color: PropTypes.string,
-  onClick: PropTypes.func,
-}
-
-export default Button
