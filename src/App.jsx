ca97cc9a1bc1836ed5b98271231a2b94104951e2
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2023-05-17 11:38:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import About from './components/About'
+import { useState } from 'react'
 
-const App = () => {
-  const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([])
-
-  useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
-      setTasks(tasksFromServer)
-    }
-
-    getTasks()
-  }, [])
-
-  // Fetch Tasks
-  const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks')
-    const data = await res.json()
-
-    return data
-  }
-
-  // Fetch Task
-  const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
-    const data = await res.json()
-
-    return data
-  }
 
 export default function App() {
   const [showAddTask, setShowAddTask] = useState(true)
@@ -58,77 +28,40 @@ export default function App() {
     },*/
 ])
 
-    setTasks([...tasks, data])
+//delete task
 
-    // const id = Math.floor(Math.random() * 10000) + 1
-    // const newTask = { id, ...task }
-    // setTasks([...tasks, newTask])
-  }
-
-  // Delete Task
-  const deleteTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'DELETE',
-    })
-    //We should control the response status to decide if we will change the state or not.
-    res.status === 200
-      ? setTasks(tasks.filter((task) => task.id !== id))
-      : alert('Error Deleting This Task')
-  }
-
-  // Toggle Reminder
-  const toggleReminder = async (id) => {
-    const taskToToggle = await fetchTask(id)
-    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
-
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(updTask),
-    })
-
-    const data = await res.json()
-
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: data.reminder } : task
-      )
-    )
-  }
-
-  return (
-    <Router>
-      <div className='container'>
-        <Header
-          onAdd={() => setShowAddTask(!showAddTask)}
-          showAdd={showAddTask}
-        />
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                {showAddTask && <AddTask onAdd={addTask} />}
-                {tasks.length > 0 ? (
-                  <Tasks
-                    tasks={tasks}
-                    onDelete={deleteTask}
-                    onToggle={toggleReminder}
-                  />
-                ) : (
-                  'No Tasks To Show'
-                )}
-              </>
-            }
-          />
-          <Route path='/about' element={<About />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  )
+const deleteTask = (id) => {
+  console.log('delete',id)
+  setTasks(tasks.filter((task)=> task.id !== id))
 }
 
-export default App
+//toggle reminder
+const toggleReminder = (id) => {
+  console.log(id)
+  setTasks(tasks.map((task) => task.id === id ? { ...task,reminder: !task.reminder }:task))
+}
+
+
+const addTask = (task) => {
+  console.log(task);
+  const id = Math.floor(Math.random() * 121) + 1
+  const newTask = {id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+  return (
+    
+    <div className="container">
+    <Header onAdd={ (e) => setShowAddTask(!showAddTask) }showAdd={showAddTask}/>
+    
+    {showAddTask && <AddTask onAdd={addTask}/>}
+
+    {
+    tasks.length > 0 ? 
+    (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> )
+    : 
+    ('No Tasks')
+    }
+    </div>
+  )
+}
